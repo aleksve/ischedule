94d302a5479dc2f9e435fc33fa8a4125b26b9c3b
Aleksander Veksler <aleksve@gmail.com>
2021-05-13 12:54:29 +0200

    Added a run_loop method, which will run until stopped

--- src/ischedule.py
+++ src/ischedule.py
@@ -1,4 +1,7 @@
-from datetime import datetime, timedelta
+import time
+from datetime import timedelta
+from threading import Event
+from time import monotonic
 from typing import Callable, List, Union
 
 
@@ -6,7 +9,7 @@ class _Task:
     def __init__(self, func: Callable, interval: timedelta):
         self.func = func
         self.interval = interval
-        self.previous_call = datetime.utcnow()
+        self.previous_call = monotonic()
         self.missed_executions = 0
 
 
@@ -20,7 +23,7 @@ def schedule(func: Callable, *, interval: Union[timedelta, float]):
         interval: how often the function is called. Either a `datetime.timedelta` or a number of seconds
 
     Raises:
-        TypeError if the interval cannot be interpreted as timedelta seconds
+        TypeError: The supplied interval cannot be interpreted as timedelta seconds
     """
     if not isinstance(interval, timedelta):
         # Raises TypeError
@@ -29,15 +32,44 @@ def schedule(func: Callable, *, interval: Union[timedelta, float]):
 
 
 def run_pending():
-    t = datetime.utcnow()
+    t = monotonic()
     for task in _tasks:
-        intervals_since_last_call: float = (t - task.previous_call) / task.interval
+        intervals_since_last_call: float = (
+            t - task.previous_call
+        ) / task.interval.total_seconds()
         if intervals_since_last_call >= 1:
             i_intervals_since_last_call = int(intervals_since_last_call)
-            task.previous_call += task.interval * i_intervals_since_last_call
+            task.previous_call += (
+                task.interval.total_seconds() * i_intervals_since_last_call
+            )
             task.missed_executions += i_intervals_since_last_call - 1
             task.func()
 
 
 def reset():
     _tasks.clear()
+
+
+def run_loop(sleep_interval: Union[timedelta, float] = 0.05, stop_event: Event = None):
+    """
+    Runs the pending tasks until the event is set.
+
+    Args:
+        sleep_interval: how long to sleep between the pending task exections.
+            Checking if any tasks are pending doesn't take much processing time, but doing so without
+            any pause would still take 100% CPU time
+        stop_event: run until this event is set. If `None`, run until the program is terminated
+
+    Raises:
+        TypeError: the supplied sleep_interval cannot be interpreted as a number of seconds
+    """
+    if not isinstance(sleep_interval, timedelta):
+        sleep_interval = timedelta(seconds=sleep_interval)
+    sleep_interval = sleep_interval.total_seconds()
+
+    if stop_event is None:
+        stop_event = Event()
+
+    while not stop_event.is_set():
+        run_pending()
+        time.sleep(sleep_interval)
--- tests/test_ischedule.py
+++ tests/test_ischedule.py
@@ -1,5 +1,7 @@
 import time
+from datetime import timedelta
 from functools import partial
+from threading import Event
 
 import pytest
 
@@ -63,6 +65,9 @@ def skip_execution():
 
 
 def test_skip():
+    """
+    Test what happens when a task uses so much time that the next execution have to skip
+    """
     ischedule.schedule(
         partial(execute_N_times_and_throw_exception, N=N_seconds), interval=1
     )
@@ -74,14 +79,40 @@ def test_skip():
             time.sleep(GRANULARITY)
 
 
+def test_timedelta():
+    ischedule.schedule(
+        partial(execute_N_times_and_throw_exception, N=10),
+        interval=timedelta(milliseconds=100),
+    )
+    with pytest.raises(InterruptedError):
+        while True:
+            ischedule.run_pending()
+            time.sleep(0.1)
+
+
+stop_event = Event()
+N_task_with_event_reset = 0
+
+
+def task_with_event_reset():
+    global N_task_with_event_reset
+    if N_task_with_event_reset == 5:
+        stop_event.set()
+
+    if N_task_with_event_reset == 6:
+        raise AssertionError()
+
+    N_task_with_event_reset += 1
+
+
+def test_run_loop():
+    ischedule.schedule(task_with_event_reset, interval=timedelta(milliseconds=200))
+    ischedule.run_loop(stop_event=stop_event)
+
+
 @pytest.fixture(autouse=True)
 def reset_scheduler():
     print("reset")
     global _i
     _i = 0
     ischedule.reset()
-
-
-if __name__ == "__main__":
-    reset_scheduler()
-    test()
